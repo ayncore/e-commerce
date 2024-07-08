17a9b34279c6cba780d1e7675aa79f7b006be619
GODCarNageOP <yashvharale@gmail.com>
2024-07-08 19:00:40 +0530

    navbar

--- src/components/global/large-footer.tsx
+++ src/components/global/large-footer.tsx
@@ -1,9 +1,99 @@
-import React from 'react'
+'use client';
+import logo from '../../../public/logo.png';
+import { FaLinkedin } from 'react-icons/fa';
+import { BsInstagram } from 'react-icons/bs';
+import { BsTwitterX } from 'react-icons/bs';
+import { Twitter } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const LargeFooter = () => {
-    return (
-        <div>LargeFooter</div>
-    )
-}
+const SITEMAP = [
+  {
+    title: 'Company',
+    links: ['About Us', 'Careers', 'Our Team', 'Projects'],
+  },
+  {
+    title: 'Help Center',
+    links: ['Discord', 'Twitter', 'GitHub', 'Contact Us'],
+  },
+  {
+    title: 'Resources',
+    links: ['Blog', 'Newsletter', 'Free Products', 'Affiliate Program'],
+  },
+  {
+    title: 'Products',
+    links: ['Templates', 'UI Kits', 'Icons', 'Mockups'],
+  },
+];
 
-export default LargeFooter
+const currentYear = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer className='relative w-full bg-[#0f172a] text-white'>
+      <div className='mx-auto w-full max-w-8xl px-8'>
+        <div className='mx-auto grid w-full grid-cols-1 gap-8 py-12 md:grid-cols-2 lg:grid-cols-4 text-center'>
+          {SITEMAP.map(({ title, links }, key) => (
+            <div key={key} className='w-full'>
+              <div className='mb-4 font-bold uppercase text-[#C7AE6A]'>
+                {title}
+              </div>
+              <ul className='space-y-1'>
+                {links.map((link, key) => (
+                  <li key={key} className='font-normal text-white'>
+                    <a
+                      href='#'
+                      className='inline-block py-1 pr-2 transition-transform hover:scale-105'
+                    >
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className='flex w-full flex-col items-center justify-center border-t border-gray-400 py-4 md:flex-row md:justify-between px-10'>
+          <div className='mb-4 text-center md:mb-0'>
+            <div className='flex lg:flex-1 items-center'>
+              <Link href='/'>
+                <div className='-m-1.5 p-1'>
+                  {/* <span className="sr-only">Your Company</span> */}
+                  <Image className='h-16 w-auto' src={logo} alt='' />
+                </div>
+              </Link>
+              <div className='py-2 text-md flex items-center gap-1 font-medium'>
+                &copy; <span className='text-[#C7AE6A]'>AYNcore</span>{' '}
+                {currentYear}. All Rights Reserved.
+              </div>
+            </div>
+          </div>
+          <div className='flex gap-4 text-white sm:justify-center'>
+            <a
+              href='tel:8104799731'
+              className='opacity-80 transition-opacity hover:opacity-100'
+            >
+              <BsInstagram size={20} />
+            </a>
+
+            <a
+              href='mailto:1032201286@tcetmumbai.in'
+              className='opacity-80 transition-opacity hover:opacity-100'
+            >
+              <BsTwitterX size={20} />
+            </a>
+
+            <a
+              href='https://yash-portfolio-six.vercel.app'
+              className='opacity-80 transition-opacity hover:opacity-100'
+            >
+              <FaLinkedin size={20} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/global/large-navbar.tsx
+++ src/components/global/large-navbar.tsx
@@ -1,28 +1,57 @@
 import { navbarLinks } from '@/lib/nav-links';
+import Image from 'next/image';
 import React from 'react';
-
-
+import ayncore from '../../../public/ayncore.png';
+import logo from '../../../public/Yash harale.jpg';
+import { ShoppingCart } from 'lucide-react';
 
 const Navbar: React.FC = () => {
+  const currentuser = true;
 
+  return (
+    <nav className='flex justify-between items-center bg-[#0f172a] text-white p-4 px-20'>
+      {/* Left side: Logo */}
+      <div className='flex items-center'>
+        <h1 className='text-2xl font-bold'>
+          <Image
+            src={ayncore}
+            height={200}
+            width={200}
+            alt='AYNcore'
+            className=''
+          />
+        </h1>
+      </div>
 
-    return (
-        <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
-            {/* Left side: Logo */}
-            <div className="flex items-center">
-                <h1 className="text-2xl font-bold">Your Logo</h1>
-            </div>
-
-            {/* Right side: Navbar links */}
-            <ul className="flex space-x-4">
+      {/* Right side: Navbar links */}
+      {/* <ul className="flex space-x-4">
                 {navbarLinks.map((link, index) => (
                     <li key={index}>
                         <a href={link.url} className="hover:text-gray-300">{link.text}</a>
                     </li>
                 ))}
-            </ul>
-        </nav>
-    );
+            </ul> */}
+
+      <div className='flex items-center space-x-8'>
+        <div className='cursor-pointer'>
+          <ShoppingCart />
+        </div>
+        {!currentuser ? (
+          <button className='bg-white text-black px-4 py-2 rounded-md'>
+            Login
+          </button>
+        ) : (
+          <Image
+            src={logo}
+            alt=''
+            height={40}
+            width={40}
+            className='object-cover rounded-full cursor-pointer'
+          />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
